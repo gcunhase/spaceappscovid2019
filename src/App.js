0437d128df2b74eb1b22b6de35877a3d262adf5c
Gwena <gwena.cs@gmail.com>
2020-05-25 21:30:39 +0900

    Change icon: rocket

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   const [isLoading, setIsLoading]  = useState(false)
 
   const DEMO_KEY = 'jwIG0mMTh5mcGZT9FP8GgLxGRAJUlXpSkZ07OIdc' //Copy the NASA Key here
-  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2019-6-3&api_key=${DEMO_KEY}` // NASA URL TODO: earth_date: input from website! 
+  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2019-6-3&api_key=${DEMO_KEY}` // NASA URL TODO: earth_date: input from website!
 
   useEffect(() => {
 
@@ -39,7 +39,7 @@ const App = () => {
                   <div>
                     <img className="loader" src={'https://d2vrnm4zvhq6yi.cloudfront.net/assets/loader_puntos-df9857dfaf7eeb01c9cb2c2d1d208a8365ea4cdab85e1adeadaceff0c8f27964.gif'} alt="Loading..."/>
                   </div>
-              : response ? 
+              : response ?
                   Object.entries(response.photos).map(([key, values]) => {
                     return <div key={key} className="image-container">
                               <img key={key} className="image-rover" src={values.img_src} alt="image_from_rover"/>
